184b91bd3171f9143ba34f8438602398b427a1a8
Bruno Barros <brun.barros1@gmail.com>
2021-08-20 15:11:47 -0300

    Adds 'ToolBar'

--- app/src/main/java/com/app/clonewhatsapp/principal/PrincipalActivity.kt
+++ app/src/main/java/com/app/clonewhatsapp/principal/PrincipalActivity.kt
@@ -2,11 +2,19 @@ package com.app.clonewhatsapp.principal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.app.clonewhatsapp.R
 
 class PrincipalActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        toolbar = findViewById(R.id.toolbarPrincipal)
+        toolbar.setTitle("WhatsApp")
+        setSupportActionBar(toolbar) 
     }
 }
