ce660a7635c747055d6e7694a881ffa845bb3235
Bruno Barros <brun.barros1@gmail.com>
2021-08-24 16:24:53 -0300

    Add ContatosFragment

---
+++ app/src/main/java/com/app/clonewhatsapp/ContatosActivity.kt
@@ -0,0 +1,52 @@
+package com.app.clonewhatsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.app.clonewhatsapp.databinding.ActivityCadastroBinding
+import com.app.clonewhatsapp.databinding.ActivityConfiguracoesBinding
+import com.app.clonewhatsapp.databinding.ActivityContatosBinding
+
+class ContatosActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    lateinit var binding: ActivityContatosBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityContatosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = findViewById(R.id.toolbarPrincipal)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+
+        binding.backArrowContatos.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    //Configuração Menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_contatos,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+/*
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.menuConfiguracoes -> {
+                startActivity(Intent(this,ConfiguracoesActivity::class.java))
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+ */
+}
--- app/src/main/java/com/app/clonewhatsapp/fragment/ConversasFragment.kt
+++ app/src/main/java/com/app/clonewhatsapp/fragment/ConversasFragment.kt
@@ -1,18 +1,23 @@
 package com.app.clonewhatsapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.clonewhatsapp.ContatosActivity
 import com.app.clonewhatsapp.R
+import com.app.clonewhatsapp.databinding.FragmentConversasBinding
 
 
 class ConversasFragment : Fragment() {
 
+    lateinit var binding: FragmentConversasBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -21,7 +26,20 @@ class ConversasFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversas, container, false)
+        //return inflater.inflate(R.layout.fragment_conversas, container, false)
+
+        binding = FragmentConversasBinding.inflate(inflater, container, false)
+
+        binding.fabContatos.setOnClickListener {
+            val intent = Intent(requireContext(), ContatosActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        return binding.root
+
+
+
     }
 
 
