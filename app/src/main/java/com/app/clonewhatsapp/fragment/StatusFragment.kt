f3e8bef32845257538f69632c2b523abc1b7d83e
Bruno Barros <brun.barros1@gmail.com>
2021-09-01 11:46:52 -0300

    Update Status UI

--- app/src/main/java/com/app/clonewhatsapp/fragment/StatusFragment.kt
+++ app/src/main/java/com/app/clonewhatsapp/fragment/StatusFragment.kt
@@ -6,9 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.clonewhatsapp.R
+import com.app.clonewhatsapp.databinding.FragmentStatusBinding
+import com.app.clonewhatsapp.model.Usuario
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 
 class StatusFragment : Fragment() {
 
+    private lateinit var binding: FragmentStatusBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +29,40 @@ class StatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false)
-    }
+        binding = FragmentStatusBinding.inflate(inflater,container,false)
 
+
+        getData()
+        return binding.root
+
+
+        }
+
+    private fun getData() {
+        var uid = FirebaseAuth.getInstance().uid
+        var ref = FirebaseDatabase.getInstance().getReference("/usuarios/$uid")
+
+
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var usuario = snapshot.getValue(Usuario::class.java)!!
+
+                if (usuario?.profileImageUrl == "") {
+
+                    binding.imagemPerfilStatus.setImageResource(R.mipmap.ic_launcher)
+
+                } else {
+                    Picasso.get()
+                        .load(usuario.profileImageUrl)
+                        .into(binding.imagemPerfilStatus)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 
 }
