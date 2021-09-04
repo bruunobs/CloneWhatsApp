c89163a81cd55bb48464204b89678338c81d221a
Bruno Barros <brun.barros1@gmail.com>
2021-09-04 15:09:40 -0300

    Adding ChatAcitity e Chat UI

--- app/src/main/java/com/app/clonewhatsapp/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/app/clonewhatsapp/ui/chat/ChatActivity.kt
@@ -3,10 +3,32 @@ package com.app.clonewhatsapp.ui.chat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.clonewhatsapp.R
+import androidx.appcompat.widget.Toolbar
+import com.app.clonewhatsapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = findViewById(R.id.toolbarPrincipal)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
